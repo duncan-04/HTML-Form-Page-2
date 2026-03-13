57b6d5dc2d0eee5e0266dfde18fea456b52ed826
duncan-04 <kgatoduncan04@icloud.com>
2026-03-13 10:38:28 -0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,22 @@
+function validateForm(){
+
+let name = document.getElementById("fullname").value;
+let email = document.getElementById("email").value;
+let telephone = document.getElementById("telephone").value;
+
+let interests = document.querySelectorAll(".interest:checked");
+
+if (name === "" || email === "" || telephone === ""){
+alert("Please fill all required fields");
+return false;
+}
+
+if(interests.length === 0){
+alert("Please select at least one area of interest");
+return false;
+}
+
+alert("Application submitted successfully!");
+return true;
+
+}
